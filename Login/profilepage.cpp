97615919c806ce9cbfc1f1adc4583099dacdeb5e
cosimo265598 <s265598@studenti.polito.it>
2020-09-13 18:03:59 +0200

    fix change image, add button for modification

--- Login/profilepage.cpp
+++ Login/profilepage.cpp
@@ -13,6 +13,7 @@ ProfilePage::ProfilePage(QWidget *parent,QWebSocket* client_socket) : QMainWindo
     ui(new Ui::ProfilePage),client_socket(client_socket)
 {
     ui->setupUi(this);
+    mod=setModify(false);
     // make a query for retrive personal data
     QByteArray out;
     BuilderMessageClient::MessageSendToServer(
@@ -41,33 +42,57 @@ void ProfilePage::on_buttonBox_ok_cancel_rejected()
 
 void ProfilePage::viewDataOfClient(QJsonObject j)
 {
-
-    // METODO DA SITEMARE PER VISUALLIZAARE UNA NUOVA IMMAGINE
-
-    qDebug()<< "PROVANDO A SETTARE I VALORI";
+    QString path=QDir().tempPath()+"/avatar.png";
     bool stateicon=j.value("ico_present").toBool();
     if(stateicon){
-        //sostiruire l'immagine corrente
+        // l'immagine del profilo non è quella di default
+        auto data=j.value("ico").toString().toLatin1();
+        QPixmap p;
+        p.loadFromData(QByteArray::fromBase64(data), "PNG");
+
+        if(!p.save(path)){
+            qDebug()<<"not saved for error";
+            return;
+        }
+
+        setImageProfile(path);
     } else {
         // usare l'immagine di default
+        QString pathdefault=":/icons_pack/avatar_default.png";
+        setImageProfile(pathdefault);
+
     }
-    QFile f(":/icons_pack/avatar.png");
-    f.rename("avatar_old.png");
-
-    auto data=j.value("ico").toString().toLatin1();
-    QPixmap p;
-    p.loadFromData(QByteArray::fromBase64(data), "PNG");
-
-    if(p.save(QDir().homePath()+"/GIT/myservertest/Login/icons_pack/new_avatar_default.png"))
-        qDebug()<<"saved";
-
-    ui->label_rv->setPixmap(p);
-
-    ui->profileImage->setPixmap(p);
-
     ui->lineEdit_nick->setText(j.value("nickname").toString());
     ui->username_value->setText(j.value("username").toString());
 
 }
 
+void ProfilePage::setImageProfile(QString& path)
+{
+    QPixmap pixmap(path);
+    QPixmap scaled = pixmap.scaled(ui->profileImage->width(), ui->profileImage->height(), Qt::KeepAspectRatio,Qt::SmoothTransformation);
+    ui->profileImage->setPixmap(scaled);
 
+}
+
+bool ProfilePage::setModify(bool value)
+{
+    ui->lineEdit_nick->setEnabled(value);
+    ui->lineEdit_password->setEnabled(value);
+    ui->lineEdit_confirm_password->setEnabled(value);
+    return value;
+}
+
+
+
+void ProfilePage::on_pushButton_abilitaModifiche_clicked()
+{
+    if(mod){
+        mod=setModify(false);
+        ui->pushButton_abilitaModifiche->setText("Abilita Modifiche");
+
+    }else{
+        mod=setModify(true);
+        ui->pushButton_abilitaModifiche->setText("Disabilita Modifiche");
+    }
+}
